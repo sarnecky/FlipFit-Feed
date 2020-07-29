258d5fa232f9715916426f562c763b1564d4e2c8
Sebastian <sarnecky@outlook.com>
2020-07-29 15:00:46 +0200

    add empty controller

--- flip-fit-posts/src/app.module.ts
+++ flip-fit-posts/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/controllers/posts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController],
   providers: [AppService],
 })
 export class AppModule {}
