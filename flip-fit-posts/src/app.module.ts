ebc599e746768a174bcf0cebde74497df0aceeb3
Sebastian <sarnecky@outlook.com>
2020-07-30 09:36:13 +0200

    mongoose working

--- flip-fit-posts/src/app.module.ts
+++ flip-fit-posts/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/controllers/posts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PostsController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017')
+  ],
+  controllers: [
+    AppController,
+    PostsController
+  ],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ flip-fit-posts/src/schemas/post.schema.ts
@@ -0,0 +1,4 @@
+import mongoose from 'mongoose'
+export const PostsSchema = new mongoose.Schema({
+    image: String
+});
