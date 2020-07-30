933c35a505ff44c4349bfbd2e18ad029879e4b42
Sebastian <sarnecky@outlook.com>
2020-07-30 10:29:07 +0200

    back to continer name

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 nest start
 # build images: 
 docker build -t flip-fit-posts .
-docker run -it -p 3000:3000 flip-fit-posts
+docker run -it -p 5001:3000 flip-fit-posts
--- flip-fit-posts/src/app.module.ts
+++ flip-fit-posts/src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017')
+     MongooseModule.forRoot('mongodb://mongo:27017')
   ],
   controllers: [
     AppController,
